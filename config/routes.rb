f48ecfe8033f260738bc317b8143e6752ab6c868
rka <greenboyroshan@gmail.com>
2019-09-24 14:05:30 +0545

    created new static pages random and about me added the to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'ideas#index'	
   resources :ideas
+  # this function gets the about from the http response and the rocketsybmbol denotes to redirect to the static pages with action about
+  get 'about' => 'static_pages#about'
+  # this function takes the random and same working principle as above 
+  get ' random ' => 'static_pages#random'
+
 end
