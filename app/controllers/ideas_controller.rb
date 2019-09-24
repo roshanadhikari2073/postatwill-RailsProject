d2d43676be8d63b07eaa9310670e2410ebc483ac
rka <greenboyroshan@gmail.com>
2019-09-24 13:59:52 +0545

    added the edit and destroy action and used linked to function in the index file

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,13 +6,47 @@ class IdeasController < ApplicationController
 	end 
 
 	def create 
+    #creating a new create idea using the create function by hitting to the idea model 
 		@ideas = Idea.create(idea_params)
+    if @idea.valid?
+      #implementation
+    else 
+      #implementation 
+    end
+
 		redirect_to root_path
 	end
+
+  #editing the ideas by 
+
+  def edit
+    @idea = Idea.find(params[:id])
+  end
+
+  #updating our ideas field 
+
+  def update 
+    @ideas = Idea.find(params[:id])
+    if @ideas.update( idea_params )
+      redirect_to root_path
+    else
+      redirect_to edit_idea_path(params[:id])
+    end
+  end
+  
+  #creating the destroy or delete for our ideator
+
+  def destroy 
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    redirect_to root_path
+  end
 
     # creating a encapsulation to prevent the external user from cracking the permission 
 
 	private 
+
+   #creating private space to whitelist the parameters
 
 	def idea_params
 		params.require(:idea).permit(:description, :author)
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,5 @@
 class Idea < ApplicationRecord
-	
+	# validate the presence of the datas in the given fields in the database
+	  validates :description, :author, presence: true
 end
 	
